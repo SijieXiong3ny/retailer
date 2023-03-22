5f1efb5810790faf75f46b06a3ffd55a26a322e4
熊思捷 <xiongsijie@xiongsijiedeMacBook-Pro.local>
2023-03-22 14:02:07 -0700

    Retailer project

--- src/test/java/com/example/retailer/RetailerApplicationTests.java
+++ src/test/java/com/example/retailer/RetailerApplicationTests.java
@@ -1,31 +1,13 @@
 package com.example.retailer;
 
-import com.example.retailer.dao.CustomerDao;
-import com.example.retailer.dao.TransactionDao;
-import com.example.retailer.entity.TransactionEntity;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RetailerApplicationTests {
-    @Autowired
-    private CustomerDao customerDao;
-    @Autowired
-    private TransactionDao transactionDao;
 
     @Test
     void contextLoads() {
-        System.out.println(customerDao.getCustomerByName("Amy"));
-        System.out.println("Add before");
-        System.out.println(transactionDao.getTransactionsByCustomerName("Amy"));
-        TransactionEntity transaction = new TransactionEntity();
-        transaction.setCustomerName("Amy");
-        transaction.setAmount(1000);
-        transaction.setMonth(10);
-        transactionDao.addTransaction(transaction);
-        System.out.println("Add after");
-        System.out.println(transactionDao.getTransactionsByCustomerName("Amy"));
     }
 
 }
